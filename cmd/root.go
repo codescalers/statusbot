1108bf9bbc093c47f7a175ff3463c613e0c53ba6
Eslam-Nawara <eslamnawara44@gmail.com>
2023-11-07 15:02:42 +0200

    remove cobra docs

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -11,19 +8,17 @@ import (
 	"os"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "statusbot",
 	Short: "statusbot reports a message every day",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 
 	Run: func(cmd *cobra.Command, args []string) {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 		token, err := cmd.Flags().GetString("token")
-		if err != nil {
+		if err != nil || token == "" {
 			log.Error().Err(err).Msg("error in token")
 			return
 		}
@@ -38,23 +33,14 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		log.Fatal().Msg(err.Error())
 	}
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.statusbot.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	cobra.OnInitialize()
 	rootCmd.Flags().StringP("token", "t", "", "Enter a valid telegram bot token")
 }
